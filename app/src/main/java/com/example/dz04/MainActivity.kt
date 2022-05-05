6d66a4ff417486213cb0c0cf0d022a316774b2d1
andrejbatarilo <batarilocore@gmail.com>
2022-05-05 20:14:00 +0200

    Added preference change listener

--- app/src/main/java/com/example/dz04/MainActivity.kt
+++ app/src/main/java/com/example/dz04/MainActivity.kt
@@ -2,35 +2,71 @@ package com.example.dz04
 
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.PreferenceManager
-
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity(), PreferenceFragmentCompat.OnPreferenceStartFragmentCallback {
+
+   private lateinit var listener:SharedPreferences.OnSharedPreferenceChangeListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val ime = findViewById<TextView>(R.id.tv_ime)
-
         val adresa = findViewById<TextView>(R.id.tv_adresa)
+        val ime = findViewById<TextView>(R.id.tv_ime)
+        val odgovor = findViewById<TextView>(R.id.tv_odgovor)
 
+        listener = SharedPreferences.OnSharedPreferenceChangeListener(fun(
+            prefs: SharedPreferences,
+            key: String
+        ) {
+            println("Aktiviran listener")
 
-        var sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+            when(key){
+                 "ime"->{
+                    ime.text = "Ime univerziteta:"+ prefs.getString("ime", "Default ime")
+                 }
+                "adresa" ->{
+                    adresa.text ="Adresa univerziteta:"+ prefs.getString("adresa","Default adresa")
+                }
 
+                "odgovor" ->{
+                    val odgovorOdabran = prefs.getString("odgovor", "1")
+                    var tacno = false
+                    if(odgovorOdabran== "2")
+                         tacno = true
 
+                    odgovor.text="Odgovor je: $tacno"
+                }
+            }
 
-        ime.text = sharedPreferences.getString("ime","Default name")
-        adresa.text = sharedPreferences.getString("adresa", "Default adress")
-
-
+        })
 
 
     }
 
+
+    override fun onResume() {
+        super.onResume()
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+
+        sharedPreferences.registerOnSharedPreferenceChangeListener(listener)
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+
+        sharedPreferences.registerOnSharedPreferenceChangeListener(listener)
+    }
     override fun onPreferenceStartFragment(
         caller: PreferenceFragmentCompat?,
         pref: Preference?
@@ -44,7 +80,7 @@ class MainActivity : AppCompatActivity(), PreferenceFragmentCompat.OnPreferenceS
             .commit()
         supportFragmentManager.addOnBackStackChangedListener {
             if (supportFragmentManager.backStackEntryCount == 0) {
-                setTitle("Yes")
+                setTitle("Root preferences")
             }
         }
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
